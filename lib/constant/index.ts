e2b23195c37bc573387341015d08e6af38d959e4
Jeremy <jeremy.luo@maifar.com>
2025-04-29 17:25:17 +0800

    update

---
+++ lib/constant/index.ts
@@ -0,0 +1,17 @@
+export const WEEK_LIST = {
+    Sunday: '0b00000001',
+    Monday: '0b00000010',
+    Tuesday: '0b00000100',
+    Wednesday: '0b00001000',
+    Thursday: '0b00010000',
+    Friday: '0b00100000',
+    Saturday: '0b01000000',
+};
+
+export const REPEAT_LIST = {
+    once: '0b00000000',
+    everyday: '0b01111111',
+    MonToFri: '0b00111110',
+    SatToSun: '0b01000001',
+    customize: '0b10000000',
+};
